7d15673a4f125bf61f8d9a7ec5f76cd4c17454d3
sanjayguwaju <sanjayguwaju@gmail.com>
2021-05-02 16:17:51 +0545

    Navbar slidng from up to down working functionality completed

---
+++ script.js
@@ -0,0 +1,8 @@
+// Navbar
+const menu = document.querySelector('.menu')
+const navbar = document.querySelector('.navbar')
+
+menu.addEventListener('click',()=> {
+  navbar.classList.toggle('change')
+})
+// End o fNavbar
